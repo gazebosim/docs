8103a9a9b7e6d8c099828969ceba729678aa2ed7
Louise Poubel <louise@openrobotics.org>
2021-01-27 11:31:23 -0800

    improve colors, support versions with multiple digits, test nested groups
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- tools/scripts/build_ign.sh
+++ tools/scripts/build_ign.sh
@@ -11,8 +11,9 @@ set -o verbose
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo -e "\e[46m\e[90mProcessing [$1/$2] branch [$3]...\e[0m\e[39m"
+echo -e "\e[46m\e[30mProcessing [$1/$2] branch [$3]...\e[0m\e[39m"
 
+echo ::group::Clone and make
 git clone https://github.com/$1/$2 -b $3
 cd $2
 
@@ -23,21 +24,26 @@ mkdir build
 cd build
 cmake ../ -DBUILD_TESTING=false
 make doc
+echo ::endgroup::
 
 if [[ ! -z "$4" && "$4" != "n" ]]; then
   # Upload documentation
-  echo -e "\e[46m\e[90mUploading documentation for $3...\e[0m\e[39m"
+  echo ::group::Upload documentation
+  echo -e "\e[46m\e[30mUploading documentation for $3...\e[0m\e[39m"
   sh upload_doc.sh $4
-  echo -e "\e[46m\e[90mUploaded documentation for $3\e[0m\e[39m"
+  echo -e "\e[46m\e[30mUploaded documentation for $3\e[0m\e[39m"
+  echo ::endgroup::
 
   # Get the project version from cmake
-  version=`grep "project(.* VERSION" ../CMakeLists.txt  | grep -oP "(?<=VERSION )[0-9].[0-9].[0-9]"`
+  echo ::group::Add version
+  version=`grep "project(.* VERSION" ../CMakeLists.txt  | grep -oP "(?<=VERSION )[0-9]*.[0-9]*.[0-9]*"`
 
   # Get the libName from the second parameter
   libName=`echo "$2" | grep -oP "(?<=ign-).*"`
   libName="${libName//-/_}"
 
-  echo -e "\e[46m\e[90mAdding version [$version] for library [$libName], release date [$5]...\e[0m\e[39m"
+  echo -e "\e[46m\e[30mAdding version [$version] for library [$libName], release date [$5]...\e[0m\e[39m"
   curl -k -X POST -d '{"libName":"'"$libName"'", "version":"'"$version"'", "releaseDate":"'"$5"'","password":"'"$6"'"}' https://api.ignitionrobotics.org/1.0/versions
-  echo -e "\e[46m\e[90mAdded version [$version] for library [$libName], release date [$5]\e[0m\e[39m"
+  echo -e "\e[46m\e[30mAdded version [$version] for library [$libName], release date [$5]\e[0m\e[39m"
+  echo ::endgroup::
 fi
