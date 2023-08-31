7c7d4e417cd928afbed65292db6fc65a90d17c72
Addisu Z. Taddese <addisu@openrobotics.org>
2023-08-31 12:21:15 -0500

    Generate documentation for Harmonic
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- tools/scripts/install_common_deps.sh
+++ tools/scripts/install_common_deps.sh
@@ -14,7 +14,6 @@ sudo apt-get install -y \
 
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-nightly $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-nightly.list > /dev/null
 
 sudo apt-get update
 
@@ -23,7 +22,6 @@ sudo apt-get install -y \
   cmake \
   curl \
   doxygen \
-  g++-8 \
   git \
   pkg-config \
   ruby-dev \
