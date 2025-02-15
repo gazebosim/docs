97ec380112e0e071959084941c7a0e79129f33bf
chapulina <chapulina@users.noreply.github.com>
2025-02-15 06:20:32 +0000

    deploy: ed43c47993b9fee950e72cc364e81e2f9b93ffb6

--- api/sdformat/15/search/enumvalues_4.js
+++ api/sdformat/15/search/enumvalues_4.js
@@ -7,5 +7,5 @@ var searchData=
   ['element_5fmissing_4',['ELEMENT_MISSING',['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#ab75072b6b9ec859b4062f7f58fca8d30a73fcebf65040535fc61782f02d9c447f',1,'sdf::SDF_VERSION_NAMESPACE']]],
   ['ellipsoid_5',['ellipsoid',['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#a6a979859e16f39207510a2d46b4e51b9a5a373dc11658d7d38c0e9d29ddcefddc',1,'sdf::SDF_VERSION_NAMESPACE::ELLIPSOID'],['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#a7e1a6f4704140eca9e55281388ebf094a5a373dc11658d7d38c0e9d29ddcefddc',1,'sdf::SDF_VERSION_NAMESPACE::ELLIPSOID']]],
   ['empty_6',['EMPTY',['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#a6a979859e16f39207510a2d46b4e51b9aba2b45bdc11e2a4a6e86aab2ac693cbb',1,'sdf::SDF_VERSION_NAMESPACE']]],
-  ['err_7',['ERR',['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#a6334565116cfb4c842a030debc58a2edacd22bad976363fdd1bfbf6759fede482',1,'sdf::SDF_VERSION_NAMESPACE']]]
+  ['err_7',['err',['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#a6334565116cfb4c842a030debc58a2edacd22bad976363fdd1bfbf6759fede482',1,'sdf::SDF_VERSION_NAMESPACE::ERR'],['../namespacesdf_1_1SDF__VERSION__NAMESPACE.html#aa412b1be5b3d080303156e7da2dfd075acd22bad976363fdd1bfbf6759fede482',1,'sdf::SDF_VERSION_NAMESPACE::ERR']]]
 ];
