c3787f2c89d4865c607a59a3f3b4ca41b9622b2a
azeey <addisu@openrobotics.org>
2025-09-03 14:47:29 +0000

    Deploying to gh-pages from @ gazebosim/docs@31218539e35a7e8b537d8f62154d29945b4c713c 🚀

--- docs/ionic/_static/documentation_options.js
+++ docs/ionic/_static/documentation_options.js
@@ -1,13 +1,1 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: 'ionic',
-    LANGUAGE: 'en',
-    COLLAPSE_INDEX: false,
-    BUILDER: 'dirhtml',
-    FILE_SUFFIX: '.html',
-    LINK_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
-};
+../../all/_static/documentation_options.js
