4964ff66d50da33af92556f50b651b9c587cbed6
chapulina <chapulina@users.noreply.github.com>
2025-09-12 06:23:55 +0000

    Deploying to gh-pages from @ gazebosim/docs@6a44f4af7a1a2a2a59d4b9bf8a20f544cea93a41 🚀

--- api/sdformat/16/search/variables_1.js
+++ api/sdformat/16/search/variables_1.js
@@ -1,1 +1,5 @@
-../../15/search/variables_1.js
+var searchData=
+[
+  ['clonedelementdescriptions_0',['clonedElementDescriptions',['../classsdf_1_1SDF__VERSION__NAMESPACE_1_1ElementPrivate.html#a4b5b3444ebdcca61790073640f2ee6cc',1,'sdf::SDF_VERSION_NAMESPACE::ElementPrivate']]],
+  ['copychildren_1',['copyChildren',['../classsdf_1_1SDF__VERSION__NAMESPACE_1_1ElementPrivate.html#a557e4b7a1ec95c7758c30a20a80cad2c',1,'sdf::SDF_VERSION_NAMESPACE::ElementPrivate']]]
+];
